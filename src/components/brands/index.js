d387b8a8801770d809343bbaad5a35fc8d189170
AbdulAziz-210 <lexustoyota2002@gmail.com>
2024-10-16 12:33:10 +0500

    remove 'marquee' bugs

--- src/components/brands/index.js
+++ src/components/brands/index.js
@@ -39,6 +39,8 @@ import safia from "../../assets/images/Safia.png";
 import macBro from "../../assets/images/MacBro.png";
 import thread from "../../assets/images/thread.png";
 import Marquee from "react-fast-marquee";
+import orientFinansBank from "../../assets/images/orientfinansbank.png";
+import AnorBank from "../../assets/images/anorbank.png";
 
 const Brands = () => {
   return (
@@ -49,16 +51,29 @@ const Brands = () => {
         </p>
         <Marquee
           className="brands-images-list"
-          speed={100}
-          gradient={true}
-          gradientColor={[255, 0, 0]}
+          speed={165}
+          // gradient={true}
+          // gradientColor={[" rgb(56, 133, 163)"]}
+          // gradientWidth={150}
           pauseOnHover={true}
           autoFill={true}
         >
+          <img
+            className="brands-images"
+            src={orientFinansBank}
+            alt="orientFinansBank Brand"
+            width={461}
+          />
           <img className="brands-images" src={nt} alt="NT Brand" />
           <img className="brands-images" src={safia} alt="Safia Brand" />
           <img className="brands-images" src={macBro} alt="MacBro Brand" />
           <img className="brands-images" src={thread} alt="Thread Brand" />
+          <img
+            className="brands-images"
+            src={AnorBank}
+            alt="Anorbank Brand"
+            width={331}
+          />
         </Marquee>
       </div>
     </div>
